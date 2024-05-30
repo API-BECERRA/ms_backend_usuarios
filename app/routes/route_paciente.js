2468756b052aa93eb00c9e02d1084dc6c5a0feec
angienohava <angienohava03>
2024-05-30 09:57:14 -0500

    se volvieron a instalar los paquetes

--- app/routes/route_paciente.js
+++ app/routes/route_paciente.js
@@ -3,7 +3,7 @@ import {user} from "../message/msj.js"
 
 const rutaPaciente = Router();
 
-rutaPaciente.get("/paciente", (req,res) => {
+rutaPaciente.get("/paciente", (req, res) => {
     res.json({
         "paciente" : user.PACIENTE
     })
